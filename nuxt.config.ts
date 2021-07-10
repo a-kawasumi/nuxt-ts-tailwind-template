7dfbe00906f2eec85f272808490816d1c3506734
yumemi-kawasumi <a_kawasumi@yumemi.co.jp>
2021-07-10 11:01:16 +0900

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,37 @@
+import { NuxtConfig } from "@nuxt/types";
+
+const config: NuxtConfig = {
+  srcDir: 'src/',
+  server: {
+    port: 8000
+  },
+  head: {
+    title: "nuxt-typescript-tailwindcss-template",
+    htmlAttrs: {
+      lang: "en"
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+  plugins: [],
+  components: true,
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    "@nuxt/typescript-build",
+    // https://go.nuxtjs.dev/tailwindcss
+    "@nuxtjs/tailwindcss"
+  ],
+  modules: [],
+  tailwindcss: {
+    exposeConfig: true,
+    jit: true,
+    // tailwindチートシート
+    viewer: false
+  }
+};
+
+export default config;
