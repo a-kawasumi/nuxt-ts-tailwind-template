a718afb44d7be2062b78ed053213b78b7db134b1
yumemi-kawasumi <a_kawasumi@yumemi.co.jp>
2021-07-11 20:24:00 +0900

    fix: lint-staged

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,26 @@
 import { NuxtConfig } from "@nuxt/types";
-import { resolve } from 'path'
+import { resolve } from "path";
 
 const config: NuxtConfig = {
   ssr: false,
-  srcDir: 'src/',
+  srcDir: "src/",
   alias: {
-    '~': resolve(__dirname, './src'),
+    "~": resolve(__dirname, "./src"),
   },
   server: {
-    port: 8000
+    port: 8000,
   },
   head: {
     title: "nuxt-typescript-tailwindcss-template",
     htmlAttrs: {
-      lang: "en"
+      lang: "en",
     },
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      { hid: "description", name: "description", content: "" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   plugins: [],
   components: true,
@@ -28,28 +28,28 @@ const config: NuxtConfig = {
     // https://go.nuxtjs.dev/typescript
     "@nuxt/typescript-build",
     // https://go.nuxtjs.dev/tailwindcss
-    "@nuxtjs/tailwindcss"
+    "@nuxtjs/tailwindcss",
   ],
   modules: [],
   tailwindcss: {
     exposeConfig: true,
     jit: true,
     // tailwindチートシート
-    viewer: false
+    viewer: false,
   },
   build: {
     extend(conf, ctx) {
-       if (ctx.isDev && ctx.isClient && conf?.module) {
-          // 保存時のeslint
-          conf.module.rules.push({
-           enforce: "pre",
-           test: /\.(js|vue)$/,
-           loader: "eslint-loader",
-           exclude: /(node_modules)/
-          })
-       }
-     }
-   }
+      if (ctx.isDev && ctx.isClient && conf?.module) {
+        // 保存時のeslint
+        conf.module.rules.push({
+          enforce: "pre",
+          test: /\.(js|ts|vue)$/,
+          loader: "eslint-loader",
+          exclude: /(node_modules)/,
+        });
+      }
+    },
+  },
 };
 
 export default config;
